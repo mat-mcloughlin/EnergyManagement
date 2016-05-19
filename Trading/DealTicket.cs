6127f63214125a4aba6f41e623e065231195bb4f
Mathew McLoughlin <mathew.mcloughlin@gmail.com>
2016-05-19 22:23:31 +0100

    Creating the Aggregate

--- Trading/DealTicket.cs
+++ Trading/DealTicket.cs
@@ -24,6 +24,54 @@ namespace Trading
             Status = DealTicketStatus.Open;
         }
 
+        public void Execute()
+        {
+            Status = DealTicketStatus.Executed;
+        }
+
+        public void Reopen()
+        {
+            if (Status != DealTicketStatus.Executed)
+            {
+                throw new Exception("Deal ticket cannot be modified once it has been executed");
+            }
+            
+            Status = DealTicketStatus.Open;
+        }
+
+        public void Cancel()
+        {
+            if (Status != DealTicketStatus.Executed)
+            {
+                throw new Exception("Deal ticket cannot be modified once it has been executed");
+            }
+
+            Status = DealTicketStatus.Cancelled;
+        }
+
+        public void AdjustCostings(decimal price, string currency, int amount, string unit)
+        {
+            if (Status != DealTicketStatus.Executed)
+            {
+                throw new Exception("Deal ticket cannot be modified once it has been executed");
+            }
+
+            if (price <= 0 || string.IsNullOrWhiteSpace(currency))
+            {
+                throw new Exception("Deal ticket must have a price");
+            }
+
+            if (amount <= 0 || string.IsNullOrWhiteSpace(unit))
+            {
+                throw new Exception("Deal ticket must have an amount");
+            }
+
+            Price = price;
+            Currency = currency;
+            Amount = amount;
+            Unit = unit;
+        }
+
         public Guid Id { get; set; }
 
         public decimal Price { get; set; }
--- Trading/DealticketService.cs
+++ Trading/DealticketService.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Infrastructure;
 
 namespace Trading
 {
@@ -15,44 +14,9 @@ namespace Trading
         public void UpdateDealTicket(DealTicket dealTicket)
         {
             var currentDealTicket = _repository.GetDealTicket(dealTicket.Id);
-
-            // Cannot modify a deal ticket once its been executed.
-            if (currentDealTicket.Status == DealTicketStatus.Executed)
-            {
-                throw new Exception("Deal ticket cannot be modified once it has been executed");
-            }
-
-            if (currentDealTicket.Status == DealTicketStatus.Cancelled && dealTicket.Status == DealTicketStatus.Open)
-            {
-                currentDealTicket.Status = dealTicket.Status;
-            }
-            else
-            {
-                ValidateDealticket(dealTicket);
-                currentDealTicket.Status = dealTicket.Status;
-                currentDealTicket.Amount = dealTicket.Amount;
-                currentDealTicket.Currency = dealTicket.Currency;
-                currentDealTicket.Unit = dealTicket.Unit;
-                currentDealTicket.Price = dealTicket.Price;
-            }
-        }
-
-        static void ValidateDealticket(DealTicket dealTicket)
-        {
-            if (dealTicket.Status != DealTicketStatus.Open)
-            {
-                throw new Exception("Deal ticket must be created with a status of created.");
-            }
-
-            if (dealTicket.Price <= 0 || string.IsNullOrWhiteSpace(dealTicket.Currency))
-            {
-                throw new Exception("Deal ticket must have a price");
-            }
-
-            if (dealTicket.Amount <= 0 || string.IsNullOrWhiteSpace(dealTicket.Unit))
-            {
-                throw new Exception("Deal ticket must have an amount");
-            }
+            // No need for the all that code any more as we have methods on the object
+            // currentDealTicket.Execute();
+            // _repository.Save(currentDealTicket);
         }
     }
 }
